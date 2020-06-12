325c83f28abfd427aa634f6cec7f804cc8ddd84a
B612 <pmoros@unal.edu.co>
2020-06-11 20:34:30 -0500

    Bst implemented, Java Dequeue used for level order -TEST

--- src/analisisgeneticoiigh/Main.java
+++ src/analisisgeneticoiigh/Main.java
@@ -6,17 +6,24 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import DataStructures.Bst;
+import java.util.Arrays;
 public class Main {
   
     
     public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {        
         //Aplication.reset();
-        Aplication my_app =  new Aplication();                                                            
-        Test test = new Test(my_app);
-        test.full_user_management(10);               
+        //Aplication my_app =  new Aplication();                                                            
+        //Test test = new Test(my_app);
+        //test.full_user_management(10);               
         //my_app.save_changes();                    
         
+        Bst<Integer> my_bst = new Bst();
+        my_bst.insert(new Integer(5));
+        my_bst.insert(new Integer(4));
+        my_bst.insert(new Integer(6));
+        my_bst.insert(new Integer(1));
+        System.out.println(Arrays.toString(my_bst.traverse_levelOrder()));
     }
     
 }
